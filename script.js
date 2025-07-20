2dad2d761906f484db4882ea4d6b6ed74616b1e2
jhonryan97 <jhonryanc70@gmail.com>
2025-07-20 16:50:14 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let burgermenu = document.querySelector('.burgermenu');
+let sidebar = document.querySelector('.sidebar');
+
+
+burgermenu.addEventListener('click', function(){
+    sidebar.classList.toggle('active');
+})
+
